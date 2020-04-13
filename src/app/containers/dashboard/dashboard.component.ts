38cbdbacf5b4385c5e7ac9a35fe46b14a0c8e11f
PBRT <pierre.beard@workangel.com>
2020-04-13 22:36:16 +0100

    add last popup

--- src/app/containers/dashboard/dashboard.component.ts
+++ src/app/containers/dashboard/dashboard.component.ts
@@ -1,7 +1,13 @@
+import { interval } from "rxjs";
+import * as moment from "moment";
 import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { ProfileService } from "../../services/profile/profile.service";
 import { EventsService } from "../../services/events/events.service";
+import { DialogComponent } from "../../components/dialog/dialog.component";
+
+const GAP = 300000; // 5 minutes in ms
 
 @Component({
   selector: "app-dashboard",
@@ -9,10 +15,12 @@ import { EventsService } from "../../services/events/events.service";
   styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
+  eventDialogShown = false;
   constructor(
     private router: Router,
     private profileService: ProfileService,
-    private eventsService: EventsService
+    private eventsService: EventsService,
+    public dialog: MatDialog
   ) {
     if (profileService.isEmpty()) {
       router.navigate(["/dob"]);
@@ -21,6 +29,30 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getEvents();
+    const observable = interval(5000);
+    const subscription = observable.subscribe(() => this.handleEventDialog());
+  }
+
+  handleEventDialog() {
+    const currentDate = new Date();
+    const { upcomingEvents } = this.getEvents();
+    if (
+      upcomingEvents.length > 0 &&
+      Math.abs(upcomingEvents[0].date.getTime() - currentDate.getTime()) <
+        GAP &&
+      this.eventDialogShown === false
+    ) {
+      this.dialog.open(DialogComponent, {
+        width: "400px",
+        data: {
+          title: `Congratulations 👏`,
+          content: `You just passed your ${upcomingEvents[0].name} at ${moment(
+            upcomingEvents[0].date
+          ).format("DD-MM-YYYY, HH:mm")}`
+        }
+      });
+      this.eventDialogShown = true;
+    }
   }
 
   getEvents() {
